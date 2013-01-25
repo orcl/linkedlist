3d48b1804f4b18438c2807daef7867621bbe4965
orcl <shipengxu@guest-wireless-207-151-247-154.usc.edu>
2013-01-25 14:20:36 -0800

    second commit

--- linkedlist/main.c
+++ linkedlist/main.c
@@ -12,31 +12,69 @@
 //for linked list implementation
 //we first define a struct for it
 
-typedef struct node
+typedef struct student
 {
     int num;
-    struct node * next;
-} Node;
+    int score;
+    struct student * next;
+} STU;
 
 //now we need to define the method to create a list
-   Node* create()
+//create a linked list means we need to return a Node
+   STU* create()
 {
    //we need to have a head node
-    Node* head = NULL;
-    head = (Node *)malloc(sizeof(Node));
+    STU* head = NULL;
+    STU* tail = NULL;
+    head = (STU *)malloc(sizeof(STU));
     if (head == NULL)
     {
         printf("Sorry, node applied failure.");
         return NULL;
     }
+    head->next = NULL;
+    tail = head;
+    STU* pNewElement = NULL;
+    int n,s;
+    while (1)
+    {
+        printf("please enter the student's number and score.\n");
+        scanf("%d%d", &n, &s);
+        if (n>0 && s>0)
+        {
+            pNewElement = (STU*)malloc(sizeof(STU));
+            if (pNewElement == NULL)
+            {
+                printf("node applied failure.");
+                return NULL;
+            }
+            
+            pNewElement->num = n;
+            pNewElement->score = s;
+            pNewElement->next = NULL;
+            tail->next = pNewElement;
+            tail = pNewElement;
+        }
+        else
+        {
+            break;
+        }
+    }//end while loop
     
+    pNewElement=head;
+    head = head->next;
+    free(pNewElement);
+    return head;
     
-    
-    
-    
-    
-}
+}//end create()
 
+
+void main(void)
+{
+    STU* head = NULL;
+    head = create();
+    display(head);
+}
 
 
 
